b7f107f9809281c5f43264504c8305ad5d4c78c2
jonathanhight24 <jonathanhight24@gmail.com>
2016-10-05 21:30:11 -0400

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def contact_us
+  @array = ["bill", "James", "micah"]
+  end
+
+  def about_us
+  end
+
+  def products
+  end
+end
